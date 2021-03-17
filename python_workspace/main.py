e54717a6d01be2d1a6c315fd65068dd46e6e5683
Ishika Samant <ishika.samant30@gmail.com>
2021-03-18 01:02:18 +0530

    summarize print and string

---
+++ python_workspace/main.py
@@ -0,0 +1,19 @@
+# ishsmnt
+
+# the print function
+print(9)
+print('String')
+print(9.89)
+print(5+6j)
+print('Ishika','kartik','Riya',end= '  ')
+print('Samant')
+print ("hey you")
+
+# numbers
+
+
+# string
+name = "Ishika Samant"
+name =''
+name = 'Ishika Samant-2'
+print(name)

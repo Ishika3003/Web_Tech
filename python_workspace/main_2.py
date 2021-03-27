328ddc9662a3e50e039dd138890f170d4e0e33e3
Ishika Samant <ishika.samant30@gmail.com>
2021-03-27 21:54:22 +0530

    Learn Type Conversion

---
+++ python_workspace/main_2.py
@@ -0,0 +1,33 @@
+n = "09"
+m = int(n)
+print(m)
+# a = input() 
+# print(type(a))
+# a = int(input("Enter a number: "))
+# print(type(a))
+# b = int(input('Enter a number: '))
+# print(type(b))
+# print(a+b)
+# print(type(a+b))
+# c = float(input("Enter a number: "))
+# print(c)
+# print(type(c))
+# d = complex(input("Enter a  number: " ))
+# print(d)
+# print(type(d))
+# num = float(input("Enter a number: "))
+# print(num)
+# print(type(num))
+# f = str(input("Enter something: "))
+# print(type(f))
+# u = list(input("Give an input: "))
+# print(u)
+# print(type(u))
+num_b = tuple(input())
+print(num_b)
+print(type(num_b))
+
+
+
+
+
